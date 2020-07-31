f6d76a7a5ad9554f69b0286d6f2f6292ddf7f753
Petr <petr.grigorev@sparm.com>
2020-07-31 21:18:25 +0300

    added server side rendering

---
+++ src/server.js
@@ -0,0 +1,26 @@
+require('@babel/register')();
+
+const express = require('express');
+const app = express();
+
+
+
+
+const render = require('./render').default;
+
+app.get('/', (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Project_React</title>
+        </head>
+        <body>
+            <div id="root">${render()}</div>
+            <script src="index.js"></script>
+        </body>
+        </html>
+    `)
+});
+app.listen(3000, () => console.log('Server start on port 3000'));
