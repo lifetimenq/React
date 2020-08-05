360c6dd60b97d682616f30d4ba36cb6bc1cd5335
Petr <petr.grigorev@sparm.com>
2020-08-05 18:58:02 +0300

    added components footer and header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
+
 import BooksList from './BooksList';
+import Header from './Header';
+import Footer from './Footer';
 
 class App extends React.Component {
     render() {
         return (
             <>
-                <header style={style.header}>
-                    cf-Book
-                </header>
-                <main style={style.main}>
-                    <BooksList books={this.props.books} />
-                </main>
-                <footer style={style.footer}>
-                    &copy; {new Date().getFullYear()}, X-com
-                </footer>
+            <Header>cf-Book</Header>
+            <main style={style.main}>
+                <BooksList books={this.props.books} />
+            </main>
+            <Footer>&copy; {new Date().getFullYear()}, X-com</Footer>
             </>
         );
     }
@@ -22,22 +21,7 @@ class App extends React.Component {
 export default App;
 
 const style = {
-    header: {
-        backgroundColor: '#2b6980',
-        color: '#fff',
-        minHeight: '50px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '1.2rem',
-        fontWeight: 'bold'
-    },
     main: {
         padding: '10px 10%'
-    },
-    footer: {
-        padding: '0 10%',
-        marginTop: '50px'
-    }
-    
+    } 
 }
