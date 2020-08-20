83bb1d8d3169bd920e9e8e47e6d577315821440e
Petr <petr.grigorev@sparm.com>
2020-08-20 19:53:37 +0300

    refactored book components

--- src/Books/Card.js
+++ src/Books/Card.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import SubscribeButton from '../Shared/SubscribeButton';
-import BookImage from './Image';
-import BookContainer from './Container';
+
 import AuthorContainer from '../Authors/Container';
 
 class Card extends React.Component {
@@ -17,10 +15,10 @@ class Card extends React.Component {
         Authors
       }
     } = this.props;
-
+    console.log(this.props.book);
     return (
       <div style={styles.container}>
-        <BookImage cover={Cover} title={Title} />
+        <Image book={this.props.book} />
         <BookContainer book={this.props.book} />
         <AuthorContainer authors={Authors} />
         <SubscribeButton>Подписаться</SubscribeButton>
@@ -31,10 +29,73 @@ class Card extends React.Component {
 
 export default Card;
 
+const Image = ({book}) => (
+  <div style={styles.imageBox}>
+    <img style={styles.image} src={book.Cover} alt={book.Title} />
+  </div>
+);
+
+const SubscribeButton = ({children}) => (
+  <div>
+    <button>{children}</button>
+</div>
+);
+
+const BookContainer = ({book}) => (
+  <div style={styles.cardBody}>
+    { (book.Subscribers > 20) &&
+      <Famous>Популярная книга!</Famous>
+    }
+    <Info book={book} />
+  </div>
+);
+
+const Famous = ({children}) => (
+  <div>{children}</div>
+);
+
+const Info = ({book}) => (
+  <>
+    <div style={styles.title}>{book.Title}</div>
+    <div style={styles.description}>Краткое описание: {book.Description}</div>
+    <div>Количество страниц: {book.NumbersOfPage}</div>
+    <div>Язык: {book.Language}</div>
+    <div>Процент прогресса: {book.Progress}%</div>
+    <div>Минимальная цена: {book.MinimumPrice} руб.</div>
+    <div>Желаемая цена: {book.SuggestedPrice} руб.</div>
+    <div>Собранная сумма: {book.CollectedAmount} руб.</div>
+    <div>Ожидаемая сумма: <span>{book.ExpectedAmount}</span> руб.</div>
+  </>
+);
 
 const styles = {
   container: {
     display: 'flex',
     marginBottom: '30px'
+  },
+  imageBox: {
+    flexBasis: '300px'
+  },
+  image: {
+    width: '100%'
+  },
+  cardBody: {
+    flex: '3',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '15px 10px'
+  },
+  description: {
+    margin: '20px 0',
+    flexBasis: '100px'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: '1.1rem'
+  },
+  authors: {
+    flex: '2',
+    display: 'flex',
+    flexDirection: 'column',
   }
 };
